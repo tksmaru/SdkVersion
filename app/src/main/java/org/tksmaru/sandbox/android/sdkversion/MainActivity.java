19f236f11b4a55703cd40cd69af9e992b38f9008
tksmaru <tksmaru@gmail.com>
2015-08-08 11:53:48 +0900

    Notification sample

--- app/src/main/java/org/tksmaru/sandbox/android/sdkversion/MainActivity.java
+++ app/src/main/java/org/tksmaru/sandbox/android/sdkversion/MainActivity.java
@@ -1,9 +1,18 @@
 package org.tksmaru.sandbox.android.sdkversion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +21,100 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        /*
+         * API level 21 以上でのみ正常に動作する
+         * (上記未満のAPIレベルで動かすと停止する）
+         */
+        Button button1 = (Button) this.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Context context = getApplicationContext();
+                Intent bootIntent = new Intent(context, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, bootIntent, 0);
+
+                Notification notification = new Notification.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("notification type 1")
+                        .setContentText("this notification works fine on only over API level 21")
+                        .setContentIntent(pendingIntent)
+                        // 警告が出る箇所
+                        .setColor(Color.RED)
+                        .build();
+
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(1, notification);
+            }
+        });
+
+        /*
+         * API level 15までのAPIのみで構成
+         */
+        Button button2 = (Button) this.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Context context = getApplicationContext();
+                Intent bootIntent = new Intent(context, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, bootIntent, 0);
+
+                Notification notification = new Notification.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("notification type 2")
+                        .setContentText("this notification works fine on over API level 15")
+                        .setContentIntent(pendingIntent)
+                        .getNotification();
+
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(2, notification);
+            }
+        });
+
+        /*
+         * API level によって呼び分ける
+         */
+        Button button3 = (Button) this.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Context context = getApplicationContext();
+                Intent bootIntent = new Intent(context, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, bootIntent, 0);
+
+                Notification notification;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    // API level 21 以上
+                    notification = new Notification.Builder(context)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("notification type 3-1")
+                            .setContentText("this notification works if API level is over 21")
+                            .setContentIntent(pendingIntent)
+                            .setColor(Color.RED) // API level 21 から追加
+                            .build(); // API level 16 から追加
+                } else {
+                    // API level 20 以下
+                    notification = new Notification.Builder(context)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("notification type 3-2")
+                            .setContentText("this notification works if API level is under 21")
+                            .setContentIntent(pendingIntent)
+                            .getNotification();
+                }
+
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(3, notification);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
